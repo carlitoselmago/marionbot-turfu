1a703851ffe490c6efd9e189792e50f31cc86aaf
carlitoselmago <ccarbonell@gmail.com>
2021-10-08 17:12:51 +0200

    Update config.py

--- transfchatbot/config.py
+++ transfchatbot/config.py
@@ -30,22 +30,8 @@ DROPOUT = 0.1
 
 EPOCHS = 300
 """
-
 #basic
 # Maximum sentence length
-MAX_LENGTH = 30#30
-
-BUFFER_SIZE = 20000
-
-# For Transformer
-NUM_LAYERS = 3#3#12#6#2
-D_MODEL =512#256#512#256
-NUM_HEADS = 8
-UNITS = 1048#512#2048 #512
-DROPOUT = 0.1
-
-EPOCHS = 120
-"""
 MAX_LENGTH = 30#30
 
 BUFFER_SIZE = 20000
@@ -58,4 +44,3 @@ UNITS = 512#2048 #512
 DROPOUT = 0.1
 
 EPOCHS = 150
-"""
